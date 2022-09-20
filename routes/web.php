019e37439d9c9e20e120c9c40fe569fb41538dd1
hoang2912002 <hoang2912002@gmail.com>
2022-09-20 20:01:35 +0700

    Lavarel

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/{ten}/{tuoi}', function ($ten,$tuoi) {
+    //-Trong laravel không có $ mà sẽ xài {} để hiểu là truyền vào 1 cái gì đó
+    //--{ten} là lấy dữ liệu từ trên thanh địa chỉ về sau đó truyền vào function 
+    //--với biến là $ten rồi in ra 
+    echo "Welcome to Laravel - $ten, $tuoi";
+
+    // return view('welcome');
+    //welcome là tên của file luôn
+});
